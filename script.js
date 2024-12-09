6b6625049887a10852ff15fa71a533efc89ac682
angelitoo <apadillaperez@iesmordefuentes.com>
2024-12-09 20:03:51 +0100

    prueba

---
+++ script.js
@@ -0,0 +1,7 @@
+// Autor: angelitooDev
+document.getElementById('actionButton').addEventListener('click', () => {
+    const message = document.getElementById('message');
+    message.textContent = '¡Git Pages funcionando correctamente!';
+    message.style.fontWeight = 'bold';
+    message.style.color = '#f39c12';
+});
